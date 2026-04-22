00103ae1391571d7f0f94d6672c94e9ed9a0ce6a
Elisa Araujo Cristo <105324780+elisaacristo@users.noreply.github.com>
2026-04-22 23:31:14 +0000

    Resoluções aula 3

--- Aula 3 - Arrays, matrizes e strings/make_target.cpp
+++ Aula 3 - Arrays, matrizes e strings/make_target.cpp
@@ -1,2 +1,29 @@
 // https://atcoder.jp/contests/abc395/tasks/abc395_b
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int lin = 1; lin <= N; lin++) {
+        for (int col = 1; col <= N; col++) {
+
+            int cima = lin;
+            int baixo = N - lin + 1;
+            int esq = col;
+            int dir = N - col + 1;
+
+            int camada = cima;
+            if (baixo < camada) camada = baixo;
+            if (esq < camada) camada = esq;
+            if (dir < camada) camada = dir;
+
+            if (camada % 2 == 1) cout << '#';
+            else cout << '.';
+        }
+        cout << "\n";
+    }
+    return 0;
+}
--- Aula 3 - Arrays, matrizes e strings/skibidus_and_ohio.cpp
+++ Aula 3 - Arrays, matrizes e strings/skibidus_and_ohio.cpp
@@ -1,2 +1,34 @@
 // https://codeforces.com/contest/2065/problem/B
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> r(n);
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+
+        bool ok = false;
+
+        for (int j = 0; j < s.size() - 1; j++) {
+            if (s[j] == s[j + 1]) {
+                ok = true;
+                break; 
+            }
+        }
+
+        if (ok) r[i] = 1;
+        else r[i] = s.size();
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << r[i] << "\n";
+    }
+
+    return 0;
+}
